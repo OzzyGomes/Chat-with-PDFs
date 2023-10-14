7f2c3a5a69ca3a3fdec1b308b9999bd9b02a0bd6
OzzyGomes <ozzygomes@outlook.com>
2023-10-14 01:09:17 -0300

    some functions

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
 import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+
+
+def get_pdf_text(pdf_docs):
+    text = []
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text.append(page.extract_text())
+    return "".join(text)
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter( 
+        separator="\n", 
+        chunk_size=1000, 
+        chunk_overlap=200, 
+        length_function=len
+        )
+    chunks = text_splitter.split_text(text)
+    return chunks
 
 
 
 def main():
+    load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
 
     st.header("Chat with multiple PDFs :books:")
@@ -11,8 +34,17 @@ def main():
 
     with st.sidebar:
         st.subheader("Seus Documentos")
-        st.file_uploader("Suba seu PDF aqui e click em 'Process': ")
-        st.button("Process")
+        pdf_docs = st.file_uploader(
+            "Suba seu PDF aqui e click em 'Process': ", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                #get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+                #st.write(raw_text)
+                #get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+                st.write(text_chunks)
+                #create vector store
 
 
 
