82604fee5f96d9c01ba5a2d030e0b846ac5c78a1
OzzyGomes <ozzygomes@outlook.com>
2023-10-14 18:36:17 -0300

    add some functions

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
 
 def get_pdf_text(pdf_docs):
     text = []
@@ -23,15 +26,29 @@ def get_text_chunks(text):
     chunks = text_splitter.split_text(text)
     return chunks
 
+
 def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings()
-    vectorstore = FAISS.from_texts(texts=text_chunks, embeddings=embeddings)
+    # embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory)
+    return conversation_chain
 
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+
 
     st.header("Chat with multiple PDFs :books:")
 
@@ -45,14 +62,17 @@ def main():
             with st.spinner("Processing"):
                 #get pdf text
                 raw_text = get_pdf_text(pdf_docs)
-                #st.write(raw_text)
+                
                 #get the text chunks
                 text_chunks = get_text_chunks(raw_text)
-                #st.write(text_chunks)
+                
                 #create vector store
                 vectorstore = get_vectorstore(text_chunks)
 
+                #create conversation chain
+                st.session_state.conversation = get_conversation_chain(vectorstore)
 
+    st.session_state.conversation
 
 if __name__ == '__main__':
     main()
