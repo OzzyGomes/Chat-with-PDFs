ca32c7f6a9d4814d3fd5a12dcd9b6608121143e4
OzzyGomes <ozzygomes@outlook.com>
2023-10-14 20:09:38 -0300

    CSS templates

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from langchain.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
+from htmlTemplates import css, bot_template, user_template
 
 def get_pdf_text(pdf_docs):
     text = []
@@ -28,13 +29,13 @@ def get_text_chunks(text):
 
 
 def get_vectorstore(text_chunks):
-    # embeddings = OpenAIEmbeddings()
-    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    embeddings = OpenAIEmbeddings()
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
 def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI
+    llm = ChatOpenAI()
     memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
@@ -42,17 +43,38 @@ def get_conversation_chain(vectorstore):
         memory=memory)
     return conversation_chain
 
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True) 
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content),unsafe_allow_html=True)
+
+
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
 
+    st.write(css, unsafe_allow_html=True)
+
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
 
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
 
     st.header("Chat with multiple PDFs :books:")
 
-    st.text_input("Pergunte Algo sobre seu PDF")
+    user_question = st.text_input("Pergunte Algo sobre seu Documento")
+    if user_question:
+        handle_userinput(user_question)
+
+
 
     with st.sidebar:
         st.subheader("Seus Documentos")
@@ -72,7 +94,7 @@ def main():
                 #create conversation chain
                 st.session_state.conversation = get_conversation_chain(vectorstore)
 
-    st.session_state.conversation
+    
 
 if __name__ == '__main__':
     main()
---
+++ htmlTemplates.py
@@ -0,0 +1,47 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.png">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+
+
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://img.freepik.com/psd-gratuitas/ilustracao-3d-de-uma-pessoa-com-oculos-de-sol_23-2149436188.jpg?w=740&t=st=1697324426~exp=1697325026~hmac=d9f37ce7d4c297cf51513c1e0e2edcbc7e91a4546d16cd93f7cabff46f58bf08">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
