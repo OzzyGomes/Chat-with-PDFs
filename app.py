7f4b06a57feea986e0dfae4482d1b067ca093a8b
OzzyGomes <ozzygomes@outlook.com>
2023-10-15 12:26:16 -0300

    env config

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def handle_userinput(user_question):
 
 
 def main():
-    load_dotenv()
+    load_dotenv(st.secrets["OPENAI_API_KEY"])
     st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
 
     st.write(css, unsafe_allow_html=True)
